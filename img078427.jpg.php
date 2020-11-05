290705d597655f69fe05a92e5f87cb5a77047fe4
Thryn <52488475+ThrynSec@users.noreply.github.com>
2020-11-05 16:16:07 +0100

    Add files via upload

---
+++ img078427.jpg.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>img078427.jpg (PRIVATE HOST)</title>
+		<meta charset="utf-8" />
+
+	</head>
+	
+	<body>
+	
+	<h1>404 - File not found</h1>
+			
+			<?php include 'cheh.php';?>
+
+	</body>
+</html>
